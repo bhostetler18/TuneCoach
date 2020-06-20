808fb6eb1cad13dda37128c7542c74d3446e63a4
Jamm Hostetler <bhostetler@ufl.edu>
2020-06-20 12:55:27 -0400

    explicit ctypes

--- python_bridge/test.py
+++ python_bridge/test.py
@@ -3,9 +3,17 @@ import threading
 
 def load_library():
     lib = cdll.LoadLibrary("./libPitchDetection.so")
-    lib.peek_stream.restype = c_double
+    lib.create_stream.argtypes = [c_int]
+    lib.create_stream.restype = c_void_p
+    lib.pause_stream.argtypes = [c_void_p]
+    lib.resume_stream.argtypes = [c_void_p]
+    lib.kill_stream.argtypes = [c_void_p]
+    lib.is_alive.argtypes = [c_void_p]
+    lib.is_alive.restype = c_bool
     lib.read_stream.restype = c_bool
     lib.read_stream.argtypes = [c_void_p, POINTER(c_double)]
+    lib.peek_stream.argtypes = [c_void_p]
+    lib.peek_stream.restype = c_double
     return lib
 
 class AudioThread(threading.Thread):
@@ -29,13 +37,14 @@ class Reader(threading.Thread):
         while(True):
             val = c_double()
             res = lib.read_stream(handle, byref(val))
-            if res:
+            if res and val:
                 print(val)
 
 
 if __name__ == "__main__":
     lib = load_library()
     handle = lib.create_stream(44100)
+    audio = AudioThread(handle, lib)
     reader = Reader(handle, lib)
+    audio.start()
     reader.start()
-    lib.start_stream(handle)
