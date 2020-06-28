458689f7b7507073fb707649e570044aae3022ae
Jamm Hostetler <bhostetler@ufl.edu>
2020-06-28 10:34:27 -0400

    smaller recording icon

--- gui/indicatorlight.py
+++ gui/indicatorlight.py
@@ -4,7 +4,7 @@ class IndicatorLight(tk.Canvas):
 	def __init__(self, parent, size=20):
 		super().__init__(parent, width=size, height=size)
 		self._parent = parent
-		self.light = self.create_oval(0, 0, size, size, fill='red', width=0, state=tk.HIDDEN)
+		self.light = self.create_oval(0.15*size, 0.15*size, 0.85*size, 0.85*size, fill='red', width=0, state=tk.HIDDEN)
 		self._isOn = False
 		self._flashing = False
 
