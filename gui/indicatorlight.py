81401bebae523b7b201c560778f9081039678de5
Jamm Hostetler <bhostetler@ufl.edu>
2020-06-28 10:58:15 -0400

    remove prints

--- gui/indicatorlight.py
+++ gui/indicatorlight.py
@@ -7,13 +7,13 @@ class IndicatorLight(tk.Canvas):
 		self.light = self.create_oval(0.15*size, 0.15*size, 0.85*size, 0.85*size, fill='red', width=0, state=tk.HIDDEN)
 		self._isOn = False
 		self._flashing = False
+		self._job = None
 
 		self.pausedCircle = self.create_oval(0, 0, size, size, fill='#72d2e0', width=0)
 		self.line1 = self.create_line(0.35*size, 0.35*size, 0.35*size, 0.65*size, width=size/4.5, capstyle=tk.ROUND)
 		self.line2 = self.create_line(0.65*size, 0.65*size, 0.65*size, 0.35*size, width=size/4.5, capstyle=tk.ROUND)
 
 	def start_flashing(self):
-		print("Start flashing")
 		if not self._flashing:
 			self._flashing = True
 			self.itemconfig(self.pausedCircle, state=tk.HIDDEN)
@@ -22,16 +22,13 @@ class IndicatorLight(tk.Canvas):
 			self._flash()
 
 	def _flash(self):
-		print("FLASH CALLED")
 		if self._flashing:
 			state = tk.HIDDEN if self._isOn else tk.NORMAL
 			self.itemconfig(self.light, state=state)
 			self._isOn = not self._isOn
-			print("after called")
 			self._job = self._parent.after(800, self._flash)
 
 	def stop(self):
-		print("Stop flashing")
 		self._parent.after_cancel(self._job)
 		self._flashing = False
 		self.itemconfig(self.light, state=tk.HIDDEN)
