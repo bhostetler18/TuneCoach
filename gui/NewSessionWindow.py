1516cfe3a9cfdaef0b6b4367906c5a7d5e21a3d7
Joe Gravelle <jgravelle@ufl.edu>
2020-06-29 11:43:00 -0400

    changing creatingnewsession window to check for duplicates

--- gui/NewSessionWindow.py
+++ gui/NewSessionWindow.py
@@ -7,17 +7,23 @@ from python_bridge.AudioManager import *
 class NewSessionWindow:
     def creating_a_new_session(self, mainWindow, popup, newName):
         # Cleanup Code:
-        if mainWindow.audio_manager is not None:
-            mainWindow.audio_manager.destroy()
+        x = 0
+        for session in mainWindow.practicesessionList:
+            if session._name == newName:
+                self.text_entry_label.cofigure(text = "Session already exists.\n Please enter a new session name.")
+                x += 1 
+        if x > 0:
+            if mainWindow.audio_manager is not None:
+                mainWindow.audio_manager.destroy()
         
-        # New Session Code:
-        new_session = Session(mainWindow.threshold, newName) # TODO: Make threshold setting in the popup for new session
-        mainWindow.currentPracticeSession = new_session
-        mainWindow.practiceSessionList.append(new_session)
-        mainWindow.audio_manager = AudioManager(new_session)
-        print("new session is created, it is set to our current practice session variable")
-        mainWindow.myDiagnosticObject.sessionName.configure(text=newName)
-        popup.destroy()
+            # New Session Code:
+            new_session = Session(mainWindow.threshold, newName) # TODO: Make threshold setting in the popup for new session
+            mainWindow.currentPracticeSession = new_session
+            mainWindow.practiceSessionList.append(new_session)
+            mainWindow.audio_manager = AudioManager(new_session)
+            print("new session is created, it is set to our current practice session variable")
+            mainWindow.myDiagnosticObject.sessionName.configure(text=newName)
+            popup.destroy()
 
     def __init__(self, mainWindow):
         new_sesh_window = tk.Toplevel(mainWindow.master)
@@ -43,8 +49,8 @@ class NewSessionWindow:
         create_session_label = tk.Label(top_frame, text="Create New Session", bg=background_color, fg="white")
         create_session_label.pack()
         new_sesh_window.lift(mainWindow.master)
-        text_entry_label = tk.Label(left_frame, text="Enter name of new Session", fg="white", bg=background_color)
-        text_entry_label.pack()
+        self.text_entry_label = tk.Label(left_frame, text="Enter name of new Session", fg="white", bg=background_color)
+        self.text_entry_label.pack()
         text_entry = tk.Entry(middle_frame)
         text_entry.insert(tk.END, "new-session-1")
         text_entry.pack()
