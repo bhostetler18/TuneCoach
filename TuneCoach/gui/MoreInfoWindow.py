13f7c84eb2a1a77e172a0f2de7aff0e95bac158c
Jamm Hostetler <bhostetler@ufl.edu>
2020-07-01 16:31:43 -0400

    fix more info window

--- TuneCoach/gui/MoreInfoWindow.py
+++ TuneCoach/gui/MoreInfoWindow.py
@@ -10,7 +10,8 @@ class MoreInfoWindow:
 
     def __init__(self, mainWindow):
         my_window = tk.Toplevel(mainWindow.master)
-        currentSession = mainWindow.currentPracticeSession
+        currentSession = mainWindow.session
+        data = currentSession.data
 
         topFrame = tk.Frame(my_window, bg=background_color)
         middleFrame = tk.Frame(my_window, bg=background_color)
@@ -25,18 +26,18 @@ class MoreInfoWindow:
         if currentSession is None:
             title_string += "No Session Currently Active"
         else:
-            title_string += mainWindow.currentPracticeSessionName + " Information:"
-            if currentSession._overall_count > 0:
-                avg_cents = currentSession._cents / currentSession._overall_count
+            title_string += currentSession.name + " Information:"
+            if data._overall_count > 0:
+                avg_cents = data._cents / data._overall_count
                 for i in range(12):
-                    if currentSession._pitch_count[i] == 0:
-                        final_string += (currentSession._notes[i] + " was not played/sung in the session.\n")
+                    if data._pitch_count[i] == 0:
+                        final_string += (data._notes[i] + " was not played/sung in the session.\n")
                     else:
-                        pitch_error = (100.0 * currentSession._in_tune_count[i]) / currentSession._pitch_count[i]
+                        pitch_error = (100.0 * data._in_tune_count[i]) / data._pitch_count[i]
                         final_string += ("%s was in tune for %.2f %% of the time.\n" % (
-                        currentSession._notes[i], pitch_error))
+                        data._notes[i], pitch_error))
                 final_string += "\nOverall"
-                final_string += "\nYou were in tune for %.2f %% of the time." % currentSession.get_overall()
+                final_string += "\nYou were in tune for %.2f %% of the time." % data.get_overall()
                 final_string += "\nYou were off by an average of %.2f cents." % avg_cents
             else:
                 final_string += "no input yet"
