8c8829281db33c1a676d51a692679bb168290341
Jamm Hostetler <bhostetler@ufl.edu>
2020-07-31 19:07:20 -0400

    dynamic resize font

--- TuneCoach/gui/RoundedLabel.py
+++ TuneCoach/gui/RoundedLabel.py
@@ -7,6 +7,7 @@ class RoundedLabel(tk.Canvas):
         self.current_text = text
         self._text = None
         self.color = color
+        self.font = tk.font.Font(weight='bold',size=11)
         super().__init__(parent, kwargs, bg=transparentColor, bd=0, highlightthickness=0) # bg=parent["background"]
         self.draw()
         self.bind("<Configure>", self.draw)
@@ -20,9 +21,18 @@ class RoundedLabel(tk.Canvas):
         self.delete("all")
         width = self.winfo_width()
         height = self.winfo_height()
+        self.set_font_size(width)
         self.backdrop = self.round_rectangle(0, 0, width, height, radius=30, fill=self.color)
         self._text = self.create_text(width/2, height/2, fill='#424651')
-        self.itemconfig(self._text, text=self.current_text, font=(None, 10, 'bold'))
+
+        self.itemconfig(self._text, text=self.current_text, font=self.font)
+
+    def set_font_size(self, width):
+        default_size = 11
+        self.font.config(size=default_size)
+        ratio = default_size/self.font.measure(self.current_text)
+        size = min(default_size, int(ratio*width*0.95))
+        self.font.config(size=size)
 
 
     # thanks https://stackoverflow.com/questions/44099594/how-to-make-a-tkinter-canvas-rectangle-with-rounded-corners
