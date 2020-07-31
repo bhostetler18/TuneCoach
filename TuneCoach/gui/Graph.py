b49e04561d5b52ffca31e39e04a42c338ddd917a
Joe <=>
2020-07-31 07:15:55 -0700

    fixing graph and tuner settings window visual issues

--- TuneCoach/gui/Graph.py
+++ TuneCoach/gui/Graph.py
@@ -12,13 +12,13 @@ class Graph(tk.Canvas):
         self.clear_plot()
         if numScores != 0:
             for i in range(0, numScores-1):
-                yCoord = int((1 - (score_history[i])/100) * (self.canvasHeight-50)+20)
+                yCoord = int((1 - (score_history[i])/100) * (self.canvasHeight-45)+25)
                 xCoord = int ((i)/9 * (self.canvasWidth - 45)) + 30
-                nextY = int((1-(score_history[i+1])/100)*(self.canvasHeight-50)+20)
+                nextY = int((1-(score_history[i+1])/100)*(self.canvasHeight-45)+25)
                 nextX = int((i+1)/9*(self.canvasWidth-45))+30
                 self.create_circle(xCoord, yCoord, 4)
                 self.create_line(xCoord, yCoord, nextX, nextY, tag = "points")
-            yCoord = int((1-(score_history[numScores-1])/100) * (self.canvasHeight-50)+20)
+            yCoord = int((1-(score_history[numScores-1])/100) * (self.canvasHeight-45)+25)
             xCoord = int((numScores-1)/9*(self.canvasWidth-45))+30
             self.create_circle(xCoord, yCoord,4)
 
@@ -43,23 +43,23 @@ class Graph(tk.Canvas):
         self.delete("all")
         self.canvasHeight = self.winfo_height()
         self.canvasWidth = self.winfo_width()
-        self.create_line(30, 20, 30, self.canvasHeight-20, fill = "black")
+        self.create_line(30, 25, 30, self.canvasHeight-20, fill = "black")
         self.create_line(30, self.canvasHeight-20, self.canvasWidth-15, self.canvasHeight-20, fill = "black")
-        self.create_text(self.canvasWidth/2, 10,text= "Score over Time", fill = "black")
+        self.create_text(self.canvasWidth/2, 10,text= "Accuracy Score over Time", fill = "black")
 
-        self.create_text(15, 20, text = "100", fill = "black")
-        self.create_text(15, int((self.canvasHeight-40)*.2+20),text = "80", fill = "black")
-        self.create_text(15, int((self.canvasHeight-40)*.4+20), text = "60", fill = "black")
-        self.create_text(15, int((self.canvasHeight-40)*.6+20), text = "40", fill = "black")
-        self.create_text(15, int((self.canvasHeight-40)*.8+20), text = "20", fill = "black")
+        self.create_text(15, 25, text = "100", fill = "black")
+        self.create_text(15, int((self.canvasHeight-45)*.2+25),text = "80", fill = "black")
+        self.create_text(15, int((self.canvasHeight-45)*.4+25), text = "60", fill = "black")
+        self.create_text(15, int((self.canvasHeight-45)*.6+25), text = "40", fill = "black")
+        self.create_text(15, int((self.canvasHeight-45)*.8+25), text = "20", fill = "black")
 
-        self.create_text(15, int((self.canvasHeight-40)+20), text = "0", fill = "black")
+        self.create_text(15, int((self.canvasHeight-45)+25), text = "0", fill = "black")
 
-        self.create_line(30,20, 34, 20, fill = "black")
-        self.create_line(30, int((self.canvasHeight-40)*.2+20), 34, int((self.canvasHeight-40)*.2+20), fill = "black")
-        self.create_line(30, int((self.canvasHeight-40)*.4+20), 34, int((self.canvasHeight-40)*.4+20), fill = "black")
-        self.create_line(30, int((self.canvasHeight-40)*.6+20), 34, int((self.canvasHeight-40)*.6+20), fill = "black")
-        self.create_line(30, int((self.canvasHeight-40)*.8+20), 34, int((self.canvasHeight-40)*.8+20), fill = "black")
+        self.create_line(30,25, 34, 25, fill = "black")
+        self.create_line(30, int((self.canvasHeight-45)*.2+25), 34, int((self.canvasHeight-45)*.2+25), fill = "black")
+        self.create_line(30, int((self.canvasHeight-45)*.4+25), 34, int((self.canvasHeight-45)*.4+25), fill = "black")
+        self.create_line(30, int((self.canvasHeight-45)*.6+25), 34, int((self.canvasHeight-45)*.6+25), fill = "black")
+        self.create_line(30, int((self.canvasHeight-45)*.8+25), 34, int((self.canvasHeight-45)*.8+25), fill = "black")
 
         #self.create_line(int((self.canvasWidth-45)+30), self.canvasHeight-29, int((self.canvasWidth-45)+30), self.canvasHeight-34, fill = "black")
 
