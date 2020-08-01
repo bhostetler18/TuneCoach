261ae3192a975fad85c6790ea2e6b0efd16a87c4
jennycodes <jennb206@gmail.com>
2020-08-01 01:24:48 -0400

    (In progress)

--- TuneCoach/tests/TestMainController.py
+++ TuneCoach/tests/TestMainController.py
@@ -1,14 +1,19 @@
 import unittest
 from unittest.mock import Mock
+from unittest.mock import patch
 from tkinter import *
 from collections import deque
 
+import TuneCoach.gui.MainWindow
 from TuneCoach.gui.MainController import MainController
 from TuneCoach.gui.MainWindow import MainWindow
+# from TuneCoach.gui.MainWindow import MainWindow
+# from TuneCoach.gui.MainWindow import invalid_path
 
-MainWindow = Mock()
 
-class MainControllerTest(unittest.TestCase):
+# mainWindow.sayhi = Mock(return_value="foo")
+
+class TestMainController(unittest.TestCase):
     def setUp(self):
         self.root = Tk()
         self.mainController = MainController(self.root)
@@ -24,9 +29,17 @@ class MainControllerTest(unittest.TestCase):
         actual_list = [self.mainController.threshold, self.mainController.yellow_threshold, self.mainController.paused, self.mainController.should_save, self.mainController.queue]
         self.assertListEqual(expected_list, actual_list)
 
-    def test_load_from(self):
-        self.assertEqual(False, self.mainController.should_save)
+    def test_load_from_when_path_not_valid(self):
+        # with patch.object(MainWindow, 'perform_load', return_value=(None, True)):
+        with patch.object(MainWindow, 'perform_load', return_value=(None, True)):
+        # mock_obj = Mock()
+        # mock_obj.perform_load.return_value = (None, True)
+            actual_return_value = self.mainController.load_from()
+        # self.assertEqual(3, 3)
+            self.assertEqual(False, actual_return_value)
 
 
 if __name__ == '__main__':
+    root = Tk()
+    mainController = MainController(root)
     unittest.main()
