3e6ba39ee5e29e13864d37a83b909ed9240aa6c8
jennycodes <jennb206@gmail.com>
2020-08-01 19:21:20 -0400

    add headers to TutorialWindow

--- TuneCoach/gui/TutorialWindow.py
+++ TuneCoach/gui/TutorialWindow.py
@@ -10,34 +10,51 @@ class TutorialWindow: #TODO: Style Tutorial Window so it's not ugly - Jenny
         tutorial_window.geometry()
         tutorial_window.minsize(width=590, height=480)
 
-        message = "TuneCoach is convenient PC-based tuning application that records sessions for intonation\n" \
-                  "and provides detailed feedback for your performance. \n" \
-                  "\n" \
-                  "To get started, click the \"File\" dropdown menu in the top left corner and select \n" \
-                  "\"New Practice Session\"\n" \
-                  "In the same dropdown menu, there are more options to manage your tuning sessions\n" \
-                  "(Note: The pitch detector will function upon startup, but it will be in practice mode.\n" \
-                  "This means that the tuner works but no session is started and therefore no data is saved.)\n" \
-                  "\n" \
-                  "The Pitch Detector continuously shows the note being played, as well as the intonation.\n" \
-                  "Green means you're in tune, yellow means you're a little bit out of tune, and red means\n" \
-                  "you're completely out of tune.\n" \
-                  "The bottom area shows your recently played notes, represented on a piano octave.\n" \
-                  "The Session Diagnostics area shows your overall score and actively reflected in the graph.\n" \
-                  "You can click on \"More Info\" button to display how in tune you were for each note,\n" \
-                  "the overall score, and how out of tune you were overall in cents.\n" \
-                  "\n" \
-                  "Keyboard Shortcuts:\n" \
-                  "------------------------------------------\n" \
-                  "Space: Pause or resume the session.\n" \
-                  "CTRL-N: Create a new session.\n" \
-                  "CTRL-S: Save the current session (save as if in Temporary Session).\n" \
-                  "CTRL-SHIFT-S or F12: Opens the \"Save as\" menu.\n" \
-                  "CTRL-O: Opens the menu to load an existing session.\n" \
-                  "CTRL-T: Opens the tuner options and settings window.\n" \
-                  "F1: Opens the tutorial.\n" \
-                  "F2: Opens the Frequently Asked Questions.\n"
+        about_tunecoach_text = "TuneCoach is convenient PC-based tuning application that records sessions for intonation and provides detailed \nfeedback for your performance."
+        new_to_tunecoach_text = 'New to TuneCoach?'
+        get_started_text = 'To get started, click the "File" dropdown menu in the top left corner and select "New Practice Session".\n' \
+                           'In the same dropdown menu, there are more options to manage your TuneCoach sessions.\n' \
+                           '(Note: Upon startup of TuneCoach, no data will be saved unless a session is explicitly started.)' \
+                           '\n\nThe Pitch Detector continuously shows the note being played, as well as the intonation.\n' \
+                           'A Green square means that the note is in tune, a yellow square means that the note is outside set cents threshold,\n and a red square means the note is very out of tune.\n' \
+                           '\nThe bottom area shows your recently played notes, represented on a piano octave.\n'\
+                           'The Session Diagnostics area shows your overall score and actively reflected in the graph.\n' \
+                           'You can click on "More Info" button to display how in tune you were for each note the overall score, and how out of tune\nyou were overall in cents.'
+        keyboard_shortcuts_title = 'Keyboard Shortcuts:'
 
-        tutorial_label = ttk.Label(tutorial_window, text=message)
-        tutorial_label.pack()
+        tutorial_text_style = ttk.Style()
+        tutorial_text_style.configure('TutorialTextLabel.TLabel', padding=10, width=100, anchor=tk.W)
+        about_tunecoach_text_label = ttk.Label(tutorial_window, text=about_tunecoach_text, style='TutorialTextLabel.TLabel')
+        about_tunecoach_text_label.pack()
+
+
+        tutorial_title_style = ttk.Style()
+        tutorial_title_style.configure('TutorialTitleLabel.TLabel', font="Ubuntu 15", width=73, anchor=tk.W)
+        new_to_tunecoach_text_label = ttk.Label(tutorial_window, text=new_to_tunecoach_text, style='TutorialTitleLabel.TLabel')
+        new_to_tunecoach_text_label.pack()
+
+        get_started_text_label = ttk.Label(tutorial_window, text=get_started_text, style='TutorialTextLabel.TLabel')
+        get_started_text_label.pack()
+
+        keyboard_shortcuts_title_label = ttk.Label(tutorial_window, text=keyboard_shortcuts_title, style='TutorialTitleLabel.TLabel', padding=10)
+        keyboard_shortcuts_title_label.pack()
+
+        text = ttk.Style()
+        text.configure('Text.TLabel', width=100, anchor=tk.W)
+
+        keyboard_shortcut_pairs = [
+            ('Space', 'Pause or resume the session.'),
+            ('CTRL-N', 'Create a new session.'),
+            ('CTRL-S', 'Save the current session.'),
+            ('CTRL-SHIFT-S', 'Save the current session as... '),
+            ('CTRL-O', 'Open the menu to load an existing session.'),
+            ('CTRL-T', 'Open the tuner settings window.'),
+            ('F1', 'Open the tutorial window.'),
+            ('F2', 'Open the FAQ window.\n')
+        ]
+
+        for key, action in keyboard_shortcut_pairs:
+            key_label = ttk.Label(tutorial_window, text=key +": "+ action, style='Text.TLabel')
+            key_label.pack()
+
         tutorial_window.lift(mainWindow.master)
