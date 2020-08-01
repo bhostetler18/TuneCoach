4e00c02b33d28e190018a37b0ae89f82f69e2c88
jennycodes <jennb206@gmail.com>
2020-08-01 16:46:39 -0400

    remove ok button, add padding

--- TuneCoach/gui/IntroWindow.py
+++ TuneCoach/gui/IntroWindow.py
@@ -21,52 +21,43 @@ class IntroWindow:
     def __init__(self, mainWindow):
         self.master = mainWindow.master
         introWindow = tk.Toplevel(self.master)
-        # introWindow.configure(bg=background_color)
         introWindow.geometry("520x300")
-        introWindow.minsize(width = 520, height = 300)
-        introWindow.maxsize(width = 520, height = 300)
+        introWindow.minsize(width = 520, height = 350)
+        introWindow.maxsize(width = 520, height = 350)
         intro_window_style = ttk.Style()
-        intro_window_style.configure("Intro.TFrame")#, background=background_color)
+        intro_window_style.configure("Intro.TFrame")
         topFrame = ttk.Frame(introWindow, style="Intro.TFrame")
         middleFrame = ttk.Frame(introWindow, style="Intro.TFrame")
         bottomFrame = ttk.Frame(introWindow, style="Intro.TFrame")
-        # bottomRight = tk.Frame(introWindow, bg=background_color)
-        # bottomLeft = tk.Frame(introWindow, bg=background_color)
 
         topFrame.grid(row=0, column=1, sticky="nsew")
         middleFrame.grid(row=1, column=1, sticky="nsew")
         bottomFrame.grid(row=2, column=1, sticky="nsew")
-        # bottomRight.grid(row=2, column=2, sticky="nsew")
-        # bottomLeft.grid(row=2, column=0, sticky="nsew")
 
         introWindow.grid_rowconfigure(0, weight=1)
         introWindow.grid_rowconfigure(1, weight=1)
         introWindow.grid_rowconfigure(2, weight=1)
-        # introWindow.grid_columnconfigure(0, weight=1)
-        # introWindow.grid_columnconfigure(1, weight=1)
-        # introWindow.grid_columnconfigure(2, weight=1)
 
         intro_label_style = ttk.Style()
-        intro_label_style.configure("IntroTitle.TLabel", font="Ubuntu 24")
-        introLabel = ttk.Label(topFrame, text="Welcome to Tune Coach!", anchor=tk.CENTER, style="IntroTitle.TLabel")#, fg="white", bg=background_color)
+        intro_label_style.configure("IntroTitle.TLabel", font="Ubuntu 24", padding=15)
+        introLabel = ttk.Label(topFrame, text="Welcome to Tune Coach!", anchor=tk.CENTER, style="IntroTitle.TLabel")
         introLabel.pack(side=tk.TOP)
 
-        text = "Click 'Load' if you would like to load a previous session from file. \n" \
+        text = "New to TuneCoach? Go to File -> New Session OR checkout Help -> Tutorial.\n\n" \
+               "Click 'Load Session' if you would like to load a previous session from file. \n" \
                "If you don't load an existing session, a temporary session will be used. You can choose to store this session under a name by clicking the 'Save Temporary Session As...' button.\n" \
-               "\n\n" \
+               "\n" \
                "For more information on using TuneCoach, navigate to the \"Help\" tab and then \"Tutorial\"."
 
         explain_label_style = ttk.Style()
         explain_label_style.configure("Intro.TLabel", wraplength=375)
-        explainLabel = ttk.Label(middleFrame, text=text, style="Intro.TLabel")# wraplength=350, bg=background_color, fg="white")
+        explainLabel = ttk.Label(middleFrame, text=text, style="Intro.TLabel")
         explainLabel.pack()
-        okButton = ttk.Button(bottomFrame, text="OK", command=lambda: introWindow.destroy())
-        okButton.pack(side=tk.RIGHT, padx=15, pady=20)
         loadButton = ttk.Button(bottomFrame, text="Load Session",
                                command=lambda: self.intro_load_session(mainWindow, introWindow))
-        loadButton.pack(side=tk.LEFT, padx=15, pady=20)
+        loadButton.pack(side=tk.RIGHT, padx=50, pady=20)
         newSession = ttk.Button(bottomFrame, text="Save Temporary Session As...",
                                command=lambda: self.intro_save_session_as(mainWindow, introWindow))
-        newSession.pack(side=tk.LEFT, padx=15, pady=20)
+        newSession.pack(side=tk.LEFT, padx=50, pady=20)
 
         introWindow.lift(self.master)
